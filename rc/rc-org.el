d4699b40b56231076a9581b6ee64a7f23411fe28
dantezy <zhangyet@gmail.com>
2019-08-15 11:17:19 +0800

    update org config and add deveplo.org

--- rc/rc-org.el
+++ rc/rc-org.el
@@ -3,6 +3,9 @@
 
 (use-package org
   :ensure t
+  :init
+  (setq org-todo-keywords
+  '((sequence "TODO" "IN-PROGRESS" "WAITING" "DONE")))
   :bind (("C-c l" . 'org-store-link)
 	 ("C-c a" . 'org-agenda)
 	 ("C-c c" . 'org-capture)))
