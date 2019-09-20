9d6b7339ba7e8d7a4c5908615c395914cd641234
dantezy <zhangyet@gmail.com>
2019-09-20 10:58:36 +0800

    Revert "add lang-c support from [Emacs Bootstrap](http://www.emacs-bootstrap.com/)"
    This reverts commit 7f04df5a11b37eaa1a594f092bd7ec1a43399ceb.
    
    一下子加太多 extension 还是吃不消。

--- init.el
+++ init.el
@@ -38,19 +38,10 @@ There are two things you can do about this warning:
     ("~/Repos/vigna" "~/Repos/vigna/book_review/douban")))
  '(package-selected-packages
    (quote
-    (projectile flycheck ivy multiple-cursors org-projectile smartparens undo-tree org-mode company company-lua format-all format-sql org lua-mode go-mode git-gutter+ diff-hl auto-complete avy ace-windows ace-window yasnippet magit markdown-mode moe-theme neotree use-package paradox))))
+    (org-mode company company-lua format-all format-sql org lua-mode go-mode git-gutter+ diff-hl auto-complete avy ace-windows ace-window yasnippet magit markdown-mode moe-theme neotree use-package paradox))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-
-(add-to-list 'load-path (concat user-emacs-directory "elisp"))
-
-(require 'base)
-(require 'base-extensions)
-(require 'base-functions)
-(require 'base-global-keys)
-
-(require 'lang-c)
