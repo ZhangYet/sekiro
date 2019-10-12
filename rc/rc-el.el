9e64c75130daecc7da8e9da21a7204a8ce487d49
dantezy <zhangyet@gmail.com>
2019-10-12 13:35:21 +0800

    set find-function-C-source-directory for xref

--- rc/rc-el.el
+++ rc/rc-el.el
@@ -1,5 +1,7 @@
 ;;; rc-el---
 (add-hook 'emacs-lisp-mode-hook #'rainbow-delimiters-mode)
-
+(setq source-directory "~/emacs-26.3/src")
+(if (file-directory-p "~/emacs-26.3")
+    (setq find-function-C-source-directory "~/emacs-26.3/"))
 (provide 'rc-el)
 ;;;
