2455def2804848904e8af771a17f6cd45b740019
dantezy <zhangyet@gmail.com>
2019-12-14 20:12:50 +0800

    add company-tern

--- rc/rc-js.el
+++ rc/rc-js.el
@@ -13,5 +13,15 @@
   (add-hook 'js2-mode-hook (lambda ()
 			     (add-hook 'xref-backend-functions #'xref-js2-xref-backend nil t))))
 
+(use-package company-tern
+  :ensure t
+  :init
+  (add-to-list 'company-backend 'company-tern)
+  (add-hook 'js2-mode-hook (lambda () (tern-mode t) (company-mode))))
+
+(define-key tern-mode-keymap (kbd "M-.") nil)
+(define-key tern-mode-keymap (kbd "M-,") nil)
+
+
 (provide 'rc-js)
 ;;;
